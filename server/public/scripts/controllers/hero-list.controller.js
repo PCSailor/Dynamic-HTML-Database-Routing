e5b7462f6370d05285246a8b5d4315f408890254
Luke Schlangen <lukeschlangen@gmail.com>
2017-03-13 15:16:45 -0500

    Adding heroes to DOM

--- server/public/scripts/controllers/hero-list.controller.js
+++ server/public/scripts/controllers/hero-list.controller.js
@@ -1,3 +1,13 @@
-app.controller('HeroListController', function(){
+app.controller('HeroListController', ['$http', function($http){
     console.log('Hero List Controller loaded');
-});
+    var self = this;
+    self.testVariable = 'I am a test!';
+    self.heroList = [];
+    $http({
+      method: 'GET',
+      url: '/heroes'
+    }).then(function(response){
+      console.log(response.data);
+      self.heroList = response.data;
+    });
+}]);
