f70e8afaf606b1667a8b5f35aef44b3738c4eb56
Luke Schlangen <lukeschlangen@gmail.com>
2017-03-13 15:47:51 -0500

    Spawn new hero

--- server/public/scripts/controllers/add-hero.controller.js
+++ server/public/scripts/controllers/add-hero.controller.js
@@ -1,3 +1,15 @@
-app.controller('AddHeroController', function(){
+app.controller('AddHeroController', ['$http', function($http){
     console.log('Add Hero Controller loaded');
-});
+    var self = this;
+    self.testMessage = 'Can you hear me?';
+
+    self.spawnNewHero = function(newHeroObject) {
+      $http({
+        method: 'POST',
+        url: '/heroes',
+        data: newHeroObject
+      }).then(function(response){
+        console.log(response);
+      });
+    }
+}]);
