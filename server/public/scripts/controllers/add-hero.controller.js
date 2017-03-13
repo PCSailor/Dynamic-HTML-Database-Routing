5ac7346801434dc3dd3b0a8608f5ca5a3430d40d
Luke Schlangen <lukeschlangen@gmail.com>
2017-03-13 15:50:46 -0500

    refactor to http shorthand

--- server/public/scripts/controllers/add-hero.controller.js
+++ server/public/scripts/controllers/add-hero.controller.js
@@ -4,11 +4,7 @@ app.controller('AddHeroController', ['$http', function($http){
     self.testMessage = 'Can you hear me?';
 
     self.spawnNewHero = function(newHeroObject) {
-      $http({
-        method: 'POST',
-        url: '/heroes',
-        data: newHeroObject
-      }).then(function(response){
+      $http.post('/heroes', newHeroObject).then(function(response){
         console.log(response);
       });
     }
--- server/public/scripts/controllers/hero-list.controller.js
+++ server/public/scripts/controllers/hero-list.controller.js
@@ -7,20 +7,14 @@ app.controller('HeroListController', ['$http', function($http){
   getHeroes();
 
   function getHeroes() {
-    $http({
-      method: 'GET',
-      url: '/heroes'
-    }).then(function(response){
+    $http.get('/heroes').then(function(response){
       console.log(response.data);
       self.heroList = response.data;
     });
   }
 
   self.obliterateHero = function(heroId) {
-    $http({
-      method: 'DELETE',
-      url: '/heroes/' + heroId
-    }).then(function(response){
+    $http.delete('/heroes/' + heroId).then(function(response){
       getHeroes();
     });
   }
